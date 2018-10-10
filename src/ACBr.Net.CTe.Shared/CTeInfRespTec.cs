08e30e9616795b8e2f6544c0998001613e428930
RFTD <rfteno@gmail.com>
2018-10-10 11:04:29 -0400

    [+] Ajustes relativo a NT 2018.002 infRespTec.

--- src/ACBr.Net.CTe.Shared/CTeInfRespTec.cs
+++ src/ACBr.Net.CTe.Shared/CTeInfRespTec.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// ***********************************************************************
+// Assembly         : ACBr.Net.CTe
+// Author           : Marcos Gerene
+// Created          : 09-10-2018
+//
+// Last Modified By : RFTD
+// Last Modified On : 10-10-2018
+// ***********************************************************************
+// <copyright file="CTeInfRespTec.cs" company="ACBr.Net">
+//		        		   The MIT License (MIT)
+//	     		    Copyright (c) 2016 Grupo ACBr.Net
+//
+//	 Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//	 The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//	 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 
 using System.ComponentModel;
-using ACBr.Net.Core.Extensions;
 using ACBr.Net.Core.Generics;
 using ACBr.Net.DFe.Core.Attributes;
 using ACBr.Net.DFe.Core.Serializer;
-
 
 namespace ACBr.Net.CTe
 {
@@ -27,7 +52,7 @@ namespace ACBr.Net.CTe
         [DFeElement(TipoCampo.StrNumberFill, "CNPJ", Min = 14, Max = 14, Ocorrencia = Ocorrencia.Obrigatoria)]
         public string CNPJ { get; set; }
 
-        [DFeElement(TipoCampo.Custom, "xContato", Min = 2, Max = 60, Ocorrencia = Ocorrencia.Obrigatoria)]
+        [DFeElement(TipoCampo.Str, "xContato", Min = 2, Max = 60, Ocorrencia = Ocorrencia.Obrigatoria)]
         public string XNome { get; set; }
 
         [DFeElement(TipoCampo.Str, "email", Min = 1, Max = 60, Ocorrencia = Ocorrencia.NaoObrigatoria)]
@@ -36,24 +61,12 @@ namespace ACBr.Net.CTe
         [DFeElement(TipoCampo.Str, "fone", Min = 6, Max = 14, Ocorrencia = Ocorrencia.NaoObrigatoria)]
         public string Fone { get; set; }
 
-        [DFeElement(TipoCampo.Int, "idCSRT", Min = 3, Max = 3, Ocorrencia = Ocorrencia.NaoObrigatoria)]
-        public int? IdCSRT { get; set; }
+        [DFeElement(TipoCampo.Str, "idCSRT", Min = 3, Max = 3, Ocorrencia = Ocorrencia.NaoObrigatoria)]
+        public string IdCSRT { get; set; }
 
-        [DFeElement(TipoCampo.Custom, "hashCSRT", Min = 28, Max = 28, Ocorrencia = Ocorrencia.NaoObrigatoria)]
+        [DFeElement(TipoCampo.Str, "hashCSRT", Min = 28, Max = 28, Ocorrencia = Ocorrencia.NaoObrigatoria)]
         public string HashCSRT { get; set; }
 
         #endregion Propriedades
-
-        #region Methods
-
-        private bool ShouldSerializeEmail() => !string.IsNullOrEmpty(Email);
-
-        private bool ShouldSerializeFone() => !string.IsNullOrEmpty(Fone);
-
-        private bool ShouldSerializeIdCSRT() => IdCSRT != null;
-
-        private bool ShouldSerializeHashCSRT() => !string.IsNullOrEmpty(HashCSRT);
-
-        #endregion Methods
     }
 }
